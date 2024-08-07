3cfe996bacfd3fffd341d040d05c6648934c8c5f
kimdonggyu <dkkim1004@hanmail.net>
2024-08-08 00:23:12 +0900

    coverage, html 삭제

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,13 @@
+import path from 'path';
+
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     environment: 'jsdom',
     reporters: ['html'], // vitest 결과 html 출력물 생성
+  },
+  resolve: {
+    alias: [{ find: '~', replacement: path.resolve(__dirname, '.') }],
   },
 });
