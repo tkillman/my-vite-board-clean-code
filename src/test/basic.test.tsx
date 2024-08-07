5fe0d33bc5c0cc9f23726ca6c894c87fe2c1304d
kimdonggyu <dkkim1004@hanmail.net>
2024-08-07 23:25:29 +0900

    vitest 설정

---
+++ src/test/basic.test.tsx
@@ -0,0 +1,5 @@
+import { expect, test } from 'vitest';
+
+test('테스트', () => {
+  expect(1).toBe(1);
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 import circleDependency from 'vite-plugin-circular-dependency';
 
 // https://vitejs.dev/config/
