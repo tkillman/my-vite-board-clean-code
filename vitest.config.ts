960621efec57baae8f016095e5d0b854622680c2
kimdonggyu <dkkim1004@hanmail.net>
2024-08-08 11:35:59 +0900

    test 제외 설정 추가

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,14 @@
 import path from 'path';
 
-import { defineConfig } from 'vitest/config';
+import { defineConfig, coverageConfigDefaults } from 'vitest/config';
 
 export default defineConfig({
   test: {
     environment: 'jsdom',
     reporters: ['html'], // vitest 결과 html 출력물 생성
+    coverage: {
+      exclude: ['**/html/**', ...coverageConfigDefaults.exclude], // coverage 제외대상, html 폴더는 제외
+    },
   },
   resolve: {
     alias: [{ find: '~', replacement: path.resolve(__dirname, '.') }],
