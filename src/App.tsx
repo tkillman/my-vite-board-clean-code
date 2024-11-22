d9976e320355397f9d949b0ce14d760061e1c2ac
kimdonggyu <dkkim1004@hanmail.net>
2024-11-22 22:29:50 +0900

    sentry 초기설정

--- src/App.tsx
+++ src/App.tsx
@@ -67,30 +67,10 @@ const App = () => {
 
 export default App;
 
-// Sentry.init({
-//   dsn: 'https://345f2dd5c885e5d2e30d42fffb490b69@o4508155676459008.ingest.us.sentry.io/4508155677769728',
-//   integrations: [
-//     // See docs for support of different versions of variation of react router
-//     // https://docs.sentry.io/platforms/javascript/guides/react/configuration/integrations/react-router/
-//     Sentry.reactRouterV6BrowserTracingIntegration({
-//       useEffect,
-//       useLocation,
-//       useNavigationType,
-//       createRoutesFromChildren,
-//       matchRoutes,
-//     }),
-//     Sentry.replayIntegration(),
-//   ],
-
-//   // Set tracesSampleRate to 1.0 to capture 100%
-//   // of transactions for tracing.
-//   tracesSampleRate: 1.0,
-
-//   // Set `tracePropagationTargets` to control for which URLs trace propagation should be enabled
-//   tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
-
-//   // Capture Replay for 10% of all sessions,
-//   // plus for 100% of sessions with an error
-//   replaysSessionSampleRate: 0.1,
-//   replaysOnErrorSampleRate: 1.0,
-// });
+Sentry.init({
+  dsn: 'https://3258429933038159eb72865056efeb88@o4508155676459008.ingest.us.sentry.io/4508341954412544',
+  integrations: [Sentry.browserTracingIntegration()],
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for tracing.
+  tracesSampleRate: 1.0,
+});
